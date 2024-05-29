36a41e2f5d8679237cab889fc9967b2aa4f7e468
smsibghatullah <smsibghatullah@gmail.com>
2024-05-29 12:11:02 +0500

    working

--- models/models.py
+++ models/models.py
@@ -1,10 +1,17 @@
-from odoo import models, fields, api
-from datetime import timedelta
+from collections import defaultdict
 
-
-import logging
-
-_logger = logging.getLogger(__name__)
-
-class SaleOrderInherit(models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    def get_aggregated_taxes(self):
+        aggregated_taxes = defaultdict(lambda: {'amount': 0.0, 'base': 0.0})
+        
+        for order in self:
+            for line in order.order_line:
+                for tax in line.tax_id:
+                    key = tax.name
+                    tax_amount = tax.amount / 100.0 * line.price_unit
+                    aggregated_taxes[key]['amount'] += tax_amount
+                    aggregated_taxes[key]['base'] += line.price_unit
+        
+        return aggregated_taxes
