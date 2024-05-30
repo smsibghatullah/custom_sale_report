f240265240889cc1f1c677b2882e38bd61deedf2
smsibghatullah <smsibghatullah@gmail.com>
2024-05-30 11:00:54 +0500

    working

--- models/models.py
+++ models/models.py
@@ -8,18 +8,36 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def get_aggregated_taxes(self):
-        aggregated_taxes = defaultdict(lambda: {'amount': 0.0, 'base': 0.0})
-        
-        for order in self:
-            for line in order.order_line:
-                for tax in line.tax_id:
-                    key = tax.name
-                    print(line.price_unit - line.discount_amount,"pppppppppppppppppppppppppppppppppppp")
-                    tax_amount = tax.amount / 100.0 * (line.price_unit - line.discount_amount)
-                    aggregated_taxes[key]['amount'] += tax_amount
-                    aggregated_taxes[key]['base'] += line.price_unit - line.discount_amount
-        
-        return aggregated_taxes
+            aggregated_taxes = defaultdict(lambda: {'amount': 0.0, 'base': 0.0})
+
+            for order in self:
+                for line in order.order_line:
+                    # Calculate the price after applying the fixed discount
+                    if line.discount_method == 'fix':
+                        price_after_discount = (line.price_unit * line.product_uom_qty) - line.discount_amount
+                    else:
+                        # If discount_method is not 'fix', calculate price without discount
+                        price_after_discount = line.price_unit * line.product_uom_qty
+
+                    # Compute taxes based on the discounted price
+                    taxes = line.tax_id.compute_all(price_after_discount, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
+
+                    # Update line with calculated values
+                    line.update({
+                        'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                        'price_total': taxes['total_included'],
+                        'price_subtotal': taxes['total_excluded'],
+                        'discount_amt': line.discount_amount,
+                    })
+
+                    # Aggregate taxes
+                    for tax in line.tax_id:
+                        key = tax.name
+                        tax_amount = tax.amount / 100.0 * price_after_discount
+                        aggregated_taxes[key]['amount'] += tax_amount
+                        aggregated_taxes[key]['base'] += price_after_discount
+
+            return aggregated_taxes
     
    
 
@@ -40,103 +58,3 @@ class AccountMove(models.Model):
         
         return aggregated_taxes
     
-class sale_order_line(models.Model):
-    _inherit = 'sale.order.line'
-
-    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id','discount_method','discount_amount')
-    def _compute_amount(self):
-        """
-        Compute the amounts of the SO line.
-        """
-        for line in self:
-            res_config= self.env.user.company_id
-            if res_config:
-                if res_config.tax_discount_policy == 'untax':
-                    if line.discount_type == 'line':
-                        if line.discount_method == 'fix':
-                            price = (line.price_unit * line.product_uom_qty) - line.discount_amount
-                            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                            line.write({
-                                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                                'price_total': taxes['total_included'] + line.discount_amount,
-                                'price_subtotal': taxes['total_excluded'] + line.discount_amount,
-                                'discount_amt' : line.discount_amount,
-                            })
-
-                        elif line.discount_method == 'per':
-                            price = (line.price_unit * line.product_uom_qty) * (1 - (line.discount_amount or 0.0) / 100.0)
-                            price_x = ((line.price_unit * line.product_uom_qty) - ((line.price_unit * line.product_uom_qty) * (1 - (line.discount_amount or 0.0) / 100.0)))
-                            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                            line.write({
-                                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                                'price_total': taxes['total_included'],
-                                'price_subtotal': taxes['total_excluded'],
-                                'discount_amt' : price_x,
-                            })
-                        else:
-                            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                            line.write({
-                                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                                'price_total': taxes['total_included'],
-                                'price_subtotal': taxes['total_excluded'],
-                            })
-                    else:
-                        price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                        taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                        line.write({
-                            'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                            'price_total': taxes['total_included'],
-                            'price_subtotal': taxes['total_excluded'],
-                        })
-                elif res_config.tax_discount_policy == 'tax':
-                    if line.discount_type == 'line':
-                        price_x = 0.0
-                        price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                        taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-
-                        if line.discount_method == 'fix':
-                            price_x = (taxes['total_included']) - ( taxes['total_included'] - line.discount_amount)
-                        elif line.discount_method == 'per':
-                            price_x = (taxes['total_included']) - (taxes['total_included'] * (1 - (line.discount_amount or 0.0) / 100.0))
-                        else:
-                            price_x = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-
-                        line.write({
-                            'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                            'price_total': taxes['total_included'],
-                            'price_subtotal': taxes['total_excluded'],
-                            'discount_amt' : price_x,
-                        })
-                    else:
-                        price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                        taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                        line.write({
-                            'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                            'price_total': taxes['total_included'],
-                            'price_subtotal': taxes['total_excluded'],
-                        })
-                else:
-                    price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                    taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                    
-                    line.write({
-                        'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                        'price_total': taxes['total_included'],
-                        'price_subtotal': taxes['total_excluded'],
-                    })
-            else:
-                price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                
-                line.write({
-                    'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                    'price_total': taxes['total_included'],
-                    'price_subtotal': taxes['total_excluded'],
-                })
-
-    is_apply_on_discount_amount =  fields.Boolean("Tax Apply After Discount")
-    discount_method = fields.Selection([('fix', 'Fixed'), ('per', 'Percentage')], 'Discount Method')
-    discount_type = fields.Selection(related='order_id.discount_type', string="Discount Applies to")
-    discount_amount = fields.Float('Discount Amount')
-    discount_amt = fields.Float('Discount Final Amount', compute="_compute_amount", store=True)
