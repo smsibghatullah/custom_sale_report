39a48e06a3d083a61b0f7402d390a94104aa0bd7
mubeen1020 <m.mubeen1020@gmail.com>
2024-06-28 18:49:20 +0500

    working

--- models/models.py
+++ models/models.py
@@ -8,51 +8,47 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
 
-    def subtract_discount_from_tax(self):
-       
-        if self.env.context.get('skip_subtract_discount_from_tax'):
-           return
+def subtract_discount_from_tax(self):
+    if self.env.context.get('skip_subtract_discount_from_tax'):
+        return
 
-        for order in self:
-            total_tax = 0 
-            discount_amt = 0
-            for line in order.order_line:
-                if line.discount_method == 'fix':
-                    price_after_discount = (line.price_unit * line.product_uom_qty) - line.discount_amount
-                elif line.discount_method == 'per':
-                    price_after_discount = (line.price_unit * line.product_uom_qty) * (1 - (line.discount_amount or 0.0) / 100.0)
-                    discount_amt = (line.price_unit * line.product_uom_qty) - price_after_discount
-                else:
-                    price_after_discount = line.price_unit * line.product_uom_qty
-                    discount_amt = 0.0
+    for order in self:
+        total_tax = 0
+        discount_amt = 0
+        order_untaxed_after_discount = order.amount_untaxed  # Initialize with untaxed amount
 
-                taxes = line.tax_id.compute_all(price_after_discount, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
-
-                line.update({
-                    'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                    'price_total': taxes['total_included'],
-                    'price_subtotal': taxes['total_excluded'],
-                })
-
-                # Calculate total tax for the order
-                total_tax += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
-            print(total_tax,"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-            if order.discount_type != 'line':
-                if order.discount_method == 'per':
-                    order.with_context(skip_subtract_discount_from_tax=True).update({
-                        # 'amount_tax': total_tax,
-                        'amount_total': (order.amount_untaxed + total_tax) - order.amount_untaxed * (order.discount_amount or 0.0) / 100.0
-                    })
-                elif order.discount_method == 'fix':
-                    order.with_context(skip_subtract_discount_from_tax=True).update({
-                        # 'amount_tax': total_tax,
-                        'amount_total': (order.amount_untaxed + total_tax) - order.discount_amount 
-                    })
+        for line in order.order_line:
+            # Calculate price after line discount
+            if line.discount_method == 'fix':
+                price_after_discount = (line.price_unit * line.product_uom_qty) - line.discount_amount
+            elif line.discount_method == 'per':
+                price_after_discount = (line.price_unit * line.product_uom_qty) * (1 - (line.discount_amount or 0.0) / 100.0)
             else:
-                order.with_context(skip_subtract_discount_from_tax=True).update({
-                    # 'amount_tax': total_tax,
-                    'amount_total': (order.amount_untaxed + total_tax) - discount_amt
-                })
+                price_after_discount = line.price_unit * line.product_uom_qty
+
+            taxes = line.tax_id.compute_all(price_after_discount, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
+
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+
+            # Calculate total tax for the order
+            total_tax += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+
+        # Subtract global discount from untaxed amount if discount type is not 'line'
+        if order.discount_type != 'line':
+            if order.discount_method == 'per':
+                order_untaxed_after_discount -= order.amount_untaxed * (order.discount_amount or 0.0) / 100.0
+            elif order.discount_method == 'fix':
+                order_untaxed_after_discount -= order.discount_amount
+
+        # Update order amounts after applying the discount
+        order.with_context(skip_subtract_discount_from_tax=True).update({
+            'amount_total': order_untaxed_after_discount + total_tax
+        })
+
 
 
 
@@ -118,26 +114,44 @@ class AccountMove(models.Model):
     _inherit = 'account.invoice'
 
     def get_aggregated_taxes(self):
-            aggregated_taxes = defaultdict(lambda: {'amount': 0.0, 'base': 0.0})
+        aggregated_taxes = defaultdict(lambda: {'amount': 0.0, 'base': 0.0})
 
-            for move in self:
-                for line in move.invoice_line_ids:
+        for order in self:
+            total_order_amount = sum(line.price_unit * line.product_uom_qty for line in order.order_line)
+
+            for line in order.order_line:
+                # Calculate price after line discount if discount type is 'line'
+                if order.discount_type == 'line':
                     if line.discount_method == 'fix':
-                        price_after_discount = (line.price_unit * line.quantity) - line.discount_amount
+                        line_discount_amount = line.discount_amount
                     elif line.discount_method == 'per':
-                        price_after_discount = (line.price_unit * line.quantity) * (1 - (line.discount_amount or 0.0) / 100.0)
+                        line_discount_amount = (line.discount_amount / 100.0) * (line.price_unit * line.product_uom_qty)
                     else:
-                        price_after_discount = line.price_unit * line.quantity
+                        line_discount_amount = 0.0
 
-                    taxes = line.invoice_line_tax_ids.compute_all(price_after_discount, move.currency_id, 1, product=line.product_id, partner=move.partner_id)
+                    price_after_discount = (line.price_unit * line.product_uom_qty) - line_discount_amount
+                    print(price_after_discount,"llllllllllllllllllllllllllllllllllllll")
 
-                    for tax in line.invoice_line_tax_ids:
-                        key = tax.name
-                        tax_amount = tax.amount / 100.0 * price_after_discount
-                        aggregated_taxes[key]['amount'] += tax_amount
-                        aggregated_taxes[key]['base'] += price_after_discount
+                # Calculate price after global discount if discount type is 'global'
+                elif order.discount_type == 'global':
+                    price_after_line_discount = line.price_unit * line.product_uom_qty
+                    proportionate_global_discount = (price_after_line_discount / total_order_amount) * order.discount_amt
+                    price_after_discount = price_after_line_discount - proportionate_global_discount
+                    print(price_after_discount,"gggggggggggggggggggggggggggggggggggggggggggg")
 
-            return aggregated_taxes
+                else:
+                    price_after_discount = line.price_unit * line.product_uom_qty
+
+                taxes = line.tax_id.compute_all(price_after_discount, line.order_id.currency_id, 1, product=line.product_id, partner=line.order_id.partner_shipping_id)
+
+                for tax in line.tax_id:
+                    print(price_after_discount,"rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+                    key = tax.name
+                    tax_amount = (tax.amount / 100.0) * price_after_discount
+                    aggregated_taxes[key]['amount'] += tax_amount
+                    aggregated_taxes[key]['base'] += price_after_discount
+
+        return aggregated_taxes
 
     def subtract_discount_from_tax(self):
             if self.env.context.get('skip_subtract_discount_from_tax'):
